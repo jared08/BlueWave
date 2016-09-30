29a56f2871439c7c76cfbc5ef42b50f14508c0bb
Red Rock <Red Rock>
2016-09-30 12:43:07 -0400

    code to fix server to allow it listen on port 80 for aws

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ var debug = require('debug')('passport-mongo');
 var app = require('./app');
 
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
 });
 
-console.log('listening on port: 3000');
+console.log('listening on port: 80');
